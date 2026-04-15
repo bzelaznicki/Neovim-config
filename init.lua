c6639d38287ffee60d632fa37e78540083a3b281
bzelaznicki <bartosz@zelaznicki.com>
2026-04-15 17:00:06 +0200

    minor bugfixes

--- init.lua
+++ init.lua
@@ -836,7 +836,10 @@ require('lazy').setup({
         automatic_installation = false,
         handlers = {
           function(server_name)
-            local server = servers[server_name] or {}
+            local server = servers[server_name]
+            if not server then
+              return
+            end
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
